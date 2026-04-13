f9d76acbfba6582e76867f354fcafc11a5da955a
octaviaazmi <octaviaazmi21@gmail.com>
2026-04-13 22:22:20 +0700

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/websapi/", // Ganti dengan nama repository kamu di GitHub
   plugins: [react()],
 })
